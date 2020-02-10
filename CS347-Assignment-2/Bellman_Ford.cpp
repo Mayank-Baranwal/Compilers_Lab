ab1368265f6030623c91116e708ca3f8223d407f
Mayank Baranwal <baranwalm2002@gmail.com>
2020-02-10 21:17:14 +0530

    Comment and alias removing

--- CS347-Assignment-2/Bellman_Ford.cpp
+++ CS347-Assignment-2/Bellman_Ford.cpp
@@ -2,40 +2,6 @@
 #define F first
 #define S second
 
-using namespace std;
-
-void relax (int u, int v, int w, int dist[]){
-    // cout<<u<<" "<<v<<"\n";
-    if (dist[u] == INT_MAX)
-        return;
-    dist[v] = min(dist[u]+w, dist[v]);
-}
-
-void bellman_ford(vector<pair<int, int>> edge[], int n, int src, int dist[]){
-    dist[src] = 0;
-    for (int i=1; i<=n; i++){
-        for (int j=1; j<=n; j++){
-            for (int k=0; k<edge[j].size(); k++)
-                relax(j,edge[j][k].F, edge[j][k].S, dist);
-        }
-    }
-}
-
-int main(){
-    int n,e,u,v,w;
-    cin>>n>>e;
-    vector<pair<int, int>> edge[n+1];
-    for (int i=1; i<=e; i++){
-        cin>>u>>v>>w;
-        edge[u].push_back({v,w});
-    }
-    int dist[n+1];
-    for (int i=1; i<=n; i++)    
-        dist[i] = INT_MAX;
-    bellman_ford(edge, n, 1, dist);
-    for (int i=1; i<=n; i++){
-        cout<<dist[i]<<" ";
-    }
-    cout<<"\n";
-
-}
+//ASDASdA
+/*SDASdASD */
+"SADFSAD"
--- CS347-Assignment-2/main.py
+++ CS347-Assignment-2/main.py
@@ -3,3 +3,40 @@ import sys, re
 out_file = open(sys.argv[2],'w')
 in_file = open(sys.argv[1]).read()
 # lines = in_file.splitlines()
+
+# def comment_substituter(current_text):
+# 	match = current_text.group(0)
+# 	if match.startswith('"') or match.startswith('/'):
+# 		return " "
+# 	else:
+# 		return match
+
+def comment_remover(current_text):
+	expr = re.compile(r'"(\\.|[^\\"])*"|/\*.*?\*/|//.*?$', re.DOTALL | re.MULTILINE)
+	comment_substituter = " "
+	modified_text = re.sub(expr, comment_substituter, current_text)
+	return modified_text
+
+# def alias_substituter(current_text):
+# 	match = current_text.group(0)
+# 	if match.startswith('#define'):
+# 		return " "
+# 	else:
+# 		return match
+
+def alias_remover(current_text):
+	expr = r'(#define .*?)[\n$]'
+	macros = re.findall(expr, current_text)
+	alias_substituter = " "
+	modified_text = re.sub(expr, alias_substituter, current_text)
+	for macro in macros:
+		macro_list = macro.split(' ')
+		key = r'\b'+macro_list[1]+r'\b'
+		replacement = ' '.join(macro_list[:2])
+		modified_text = re.sub(key, replacement, modified_text)
+	return modified_text
+
+modified_text = comment_remover(in_file)
+# print (modified_text)
+modified_text = alias_remover(modified_text)
+print (modified_text)
